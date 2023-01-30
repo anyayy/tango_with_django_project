9530ab812f8bb1187dc61a0894afb6b10f78e1bd
Anya Chaudhary <anyachau19@gmail.com>
2023-01-30 00:13:33 +0000

    Chapter 7

--- rango/models.py
+++ rango/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django import forms
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LENGTH = 128
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
@@ -18,10 +20,22 @@ class Category(models.Model):
         return self.name
 
 class Page(models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
+
+class PageForm(forms.ModelForm):
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+        return cleaned_data
